6bea3444042577dc3b27ab5e3b43a4a52b7175d7
Orange Karpusi <th3m4g3ofl1f3@gmail.com>
2016-01-10 15:57:29 -0800

    Xcode projects

---
+++ test/test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by Orange Karpusi on 1/10/16.
+//  Copyright © 2016 Orange Karpsusi. All rights reserved.
+//
+
+#include <iostream>
+
+int main() {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
