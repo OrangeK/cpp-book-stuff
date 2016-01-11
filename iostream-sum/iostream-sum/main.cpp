8953fd5938dfa28d398a78261394081cc68d59ba
Orange Karpusi <th3m4g3ofl1f3@gmail.com>
2016-01-10 16:10:10 -0800

    moving stuff around

---
+++ iostream-sum/iostream-sum/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by Orange Karpusi on 1/10/16.
+//  Copyright © 2016 Orange Karpsusi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(){
+    std::cout << "Enter two numbers:" << std::endl;
+    int v1 = 0, v2 = 0;
+    std::cin >> v1 >> v2;
+    std::cout << "The sum of " << v1 << " and " << v2 << " is " << v1 + v2 << std::endl;
+    return 0;
+}
