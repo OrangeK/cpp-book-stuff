b4a3d74a3406ad8a761b1e29e5467d7be3d026c4
Orange Karpusi <th3m4g3ofl1f3@gmail.com>
2016-02-20 11:46:41 -0800

    picking around

---
+++ draw/draw/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  draw
+//
+//  Created by Orange Karpusi on 2/20/16.
+//  Copyright © 2016 Orange Karpsusi. All rights reserved.
+//
+
+#include <iostream>
+#include "/Users/thomasmurray/Desktop/github stuff/cpp-book-stuff/SDL2.framework/Headers/SDL_rect.h"
+#include "/Users/thomasmurray/Desktop/github stuff/cpp-book-stuff/SDL2.framework/Headers/SDL_render.h"
+
+
+int main() {
+    SDL_Rect testrect;
+    testrect.x = 0;
+    testrect.y = 0;
+    testrect.h = 100;
+    testrect.w = 100;
+}
---
+++ testing stuff/testing stuff/bad math.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  testing stuff
+//
+//  Created by Orange Karpusi on 2/20/16.
+//  Copyright © 2016 Orange Karpsusi. All rights reserved.
+//
+
+#include <iostream>
+
+int main() {
+    unsigned u = 10;
+    int i = -42;
+    std::cout << i + i << std::endl;
+    std::cout << i + u << std::endl;
+    int j = i + i;
+    int v = u + i;
+    std::cout << j + v <<std::endl;
+    std::cout << "lolol im bad at math" << std::endl;
+}
+
